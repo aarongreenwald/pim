cfd526ed888eb857db6e6d935fc7125049127db2
Aaron Greenwald <aaron@aarongreenwald.com>
2015-06-29 19:13:58 -0400

    enhancements to login screen

--- src/pim/security/views.py
+++ src/pim/security/views.py
@@ -4,11 +4,13 @@ from pyramid.view import (
     )
 
 from pyramid.response import Response
+from pyramid.request import Request
 
 from pyramid.security import (
     remember,
     forget,
     authenticated_userid,
+    NO_PERMISSION_REQUIRED
     )
 
 from pyramid.httpexceptions import (
@@ -18,32 +20,34 @@ from pyramid.httpexceptions import (
 
 import auth
 
+@view_config(route_name='login', renderer='templates/login.pt', request_method='POST', permission=NO_PERMISSION_REQUIRED)
+def post_login(request):
+    username = request.params['login']
+    password = request.params['password']
+    came_from = request.params['came_from']
+    if auth.authenticate(username, password, request):
+        headers = remember(request, username)
+        return HTTPFound(location = came_from,
+                         headers = headers)
+    else:
+        request.invoke_subrequest(Request.blank('/login?came_from=' + came_from))
+
+
 #this is where forbidden views go. alternatively use a special view, see below.
 @forbidden_view_config(renderer='templates/login.pt') 
-@view_config(route_name='login', renderer='templates/login.pt')
-def login(request):    
+@view_config(route_name='login', renderer='templates/login.pt', request_method='GET')
+def get_login(request):     
     referrer = request.url
     if referrer == request.route_url('login'):
         referrer = '/' # never use the login form itself as came_from
     came_from = request.params.get('came_from', referrer)
-    message = ''
-    login = ''
-    password = ''
-    if 'form.submitted' in request.params:
-        username = request.params['login']
-        password = request.params['password']
-        if auth.authenticate(username, password, request):
-            headers = remember(request, username)
-            return HTTPFound(location = came_from,
-                             headers = headers)
-        message = 'Invalid username or password'
-
+    
     return dict(
-        message = message,
+        message = '',
         url = request.application_url + '/login',
         came_from = came_from,
-        login = login,
-        password = password,
+        login = '',
+        password = '',
         )
 
 @view_config(route_name='logout')
@@ -51,10 +55,6 @@ def logout(request):
     headers = forget(request)
     return HTTPFound(location = request.route_url('login'),
                      headers = headers)
-                     
-#@forbidden_view_config()
-#def forbidden(request):
-#    return Response('TODO')
 
 from pyramid.view import notfound_view_config
 @notfound_view_config(request_method='GET')
