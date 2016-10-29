2b27c495a6c1456fed822f579cafa7ccc81fd5fb
Aaron Greenwald <aaron@aarongreenwald.com>
2016-10-29 20:08:20 +0300

    progress on new diary

--- server/data.js
+++ server/data.js
@@ -4,7 +4,7 @@ const Chance = require('chance')
 const chance = new Chance()
 
 const randomContent = () => [].slice.apply(new Uint8Array(Math.floor(Math.random() * 10))).map(() => chance.paragraph()).join('\n\n')
-console.log(randomContent())
+
 
 const entries = [].slice.apply(new Uint8Array(50)).map(() => ({
   startTimestamp: chance.date(),
--- server/index.js
+++ server/index.js
@@ -5,12 +5,25 @@ const app = require('express')()
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+
+  console.log(new Date(), req.method, req.url)
   next()
 })
 
-app.get('/entries', (req, res) => {
-  res.send(data.getEntries())
-})
+app.route('/entries')
+    .get((req, res) => {
+        res.send(data.getEntries())
+    })    
+    .post((req, res) => {
+        res.send({startTimestamp: new Date(), content: ''})
+    })
 
+app.route('/entries/:id')
+    .get((req, res) => {
+        res.send(data.getEntry(req.params.id))
+    })
+    .put((req, res) => {
+        res.send(200)
+    })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
