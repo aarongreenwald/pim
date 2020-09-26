c28392a5874a719434fc399a4e0e121de5aff090
Aaron Greenwald <aaron@aarongreenwald.com>
2020-09-26 11:13:46 +0300

    basic spending form

--- statics/src/financials/spending.js
+++ statics/src/financials/spending.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 
 export default function Spending() {
+  const [item, setItem] = useState({
+    paidDate: new Date()
+  })
+  const updateItem = useCallback(({target}) => {
+    setItem({
+      ...item,
+      [target.name]: target.value
+    })
+  }, [item])
+
   return (
     <div>
-      Hello world
+      <form>
+        <div>
+          <label>Date</label>
+          <input type="date" onChange={updateItem} value={item.paidDate} name="paidDate"/>
+        </div>
+        <div>
+          <label>Incurred Start Date</label>
+          <input type="date" onChange={updateItem}/>
+        </div>
+        <div>
+          <label>Incurred Start Date</label>
+          <input type="date" onChange={updateItem}/>
+        </div>
+        <div>
+          <label>Incurred End Date</label>
+          <input type="date" onChange={updateItem}/>
+        </div>
+        <div>
+          <label>Counterparty</label>
+          <input type="text" onChange={updateItem}/>
+        </div>
+        <div>
+          <label>Amount</label>
+          <input type="number" onChange={updateItem}/>
+        </div>
+        <div>
+          <label>Currency</label>
+          <input type="text" onChange={updateItem}/>
+        </div>
+      </form>
     </div>
   )
 }
