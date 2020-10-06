d56bda0f04b3b7094e405aeef339bab5e120c490
Aaron Greenwald <aaron@aarongreenwald.com>
2020-10-06 19:40:03 +0300

    experiment with datagrid/table and alternative panel libraries

--- web/src/financials/finacials.home.tsx
+++ web/src/financials/finacials.home.tsx
@@ -1,12 +1,16 @@
-import {Button, Dialog} from '@material-ui/core';
+import {Button} from '@material-ui/core';
 import {useCallback, useState} from 'react';
 import {AddPayment} from './add-payment';
 import * as React from 'react';
 import {Payments} from './payments';
+import {Panel, initializeIcons} from '@fluentui/react';
+
+initializeIcons()
 
 export const FinancialsHome: React.FC = () => {
     const [showAddPayment, setShowAddPayment] = useState(false)
     const onAddPayment = useCallback(() => setShowAddPayment(!showAddPayment), [showAddPayment]);
+    const closePanel = useCallback(() => setShowAddPayment(false), [])
 
     return (
         <>
@@ -17,12 +21,17 @@ export const FinancialsHome: React.FC = () => {
                 onClick={onAddPayment}>
                 Add Payment
             </Button>
-            {/*<Drawer anchor={'right'} open={showAddPayment} onClose={() => setShowAddPayment(false)}>*/}
+            {/*<Drawer anchor={'right'} open={showAddPayment} onClose={closePanel}>*/}
             {/*    <AddPayment />*/}
             {/*</Drawer>*/}
-            <Dialog fullScreen open={showAddPayment}>
-                <AddPayment onClose={() => setShowAddPayment(false)}/>
-            </Dialog>
+            {/*<Dialog fullScreen open={showAddPayment}>*/}
+            {/*    <AddPayment onClose={closePanel}/>*/}
+            {/*</Dialog>*/}
+            <Panel
+                isOpen={showAddPayment}
+                onDismiss={closePanel}>
+                <AddPayment onClose={closePanel}/>
+            </Panel>
         </>
     )
 }
--- web/src/financials/payments.tsx
+++ web/src/financials/payments.tsx
@@ -1,7 +1,9 @@
-import {useEffect, useState} from 'react';
+import * as React from 'react';
+import {useCallback, useEffect, useMemo, useState} from 'react';
 import {Payment} from '@pim/common';
 import {getPayments} from '../services/server-api';
-import * as React from 'react';
+import {DataGrid} from '@material-ui/data-grid';
+import {DetailsList, DetailsListLayoutMode, SelectionMode} from '@fluentui/react';
 
 export const Payments: React.FC = () => {
 
@@ -10,16 +12,29 @@ export const Payments: React.FC = () => {
         getPayments().then(setPayments)
     }, [])
 
+    const sortPayments = useCallback((sort: Sort) => {
+        const newPayments = [...payments];
+        const sortFn =
+            sort.direction ?
+                (a, b) => a[sort.fieldName] > b[sort.fieldName] ? 1 : -1 :
+                (a, b) => a[sort.fieldName] < b[sort.fieldName] ? 1 : -1
+
+        newPayments.sort(sortFn)
+
+        setPayments(newPayments)
+    }, [payments])
+
     return (
-        <FinancialsTable data={payments} />
+        payments ? <List data={payments} sortData={sortPayments} /> : null
     )
 }
 
 interface TableProps {
     data: any[];
+    sortData?: (sort: Sort) => void;
 }
 
-export const FinancialsTable: React.FC<TableProps> = ({data}) => {
+export const HtmlTable: React.FC<TableProps> = ({data}) => {
     if (!data?.length) {
         return null;
     }
@@ -51,5 +66,65 @@ export const FinancialsTable: React.FC<TableProps> = ({data}) => {
             }
             </tbody>
         </table>
+    )
+}
+
+interface Sort {
+    fieldName: string;
+    direction: number; //TODO name these
+}
+
+const defaultSortDirection = (fieldName: string): number => 0
+
+export const List: React.FC<TableProps> = ({data, sortData}) => {
+    const [sort, setSort] = useState<Sort>(null)
+    const columns = useMemo(() => {
+        const keys = Object.keys(data[0]);
+        return keys.map(key => ({
+            key,
+            minWidth: 100,
+            fieldName: key,
+            name: key,
+            isResizable: true,
+            isSorted: sort?.fieldName === key,
+            isSortedDescending: sort?.direction === 0,
+            // isCollapsible: true, //what does this do?
+            onColumnClick: () => {
+                const newSort = {
+                    fieldName: key,
+                    direction: sort?.fieldName === key ? ~sort?.direction : defaultSortDirection(key)
+                };
+                setSort(newSort)
+                sortData(newSort);
+            }
+        }))
+    }, [data, sort])
+
+    const items = useMemo(() => data.map(item => ({...item, id: item.payment_id})), [data])
+
+    const getKey = useCallback((item) => item.id, []);
+
+    return <DetailsList items={items}
+               getKey={getKey}
+               compact
+               layoutMode={DetailsListLayoutMode.fixedColumns}
+               selectionMode={SelectionMode.none}
+               columns={columns}/>
+}
+
+export const Grid: React.FC<TableProps> = ({data}) => {
+    const columns = useMemo(() => {
+        const keys = Object.keys(data[0]);
+        return keys.map(key => ({
+            field: key,
+            type: key.indexOf('date') ? 'date' : 'string',
+            headerName: key
+        }))
+    }, [data])
+
+    const foo = useMemo(() => data.map(item => ({...item, id: item.payment_id})), [data])
+
+    return (
+        <DataGrid rows={foo} columns={columns} />
     )
 }
