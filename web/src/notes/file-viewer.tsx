04cadbe7d31a8dd2b46d676e1deef3a583380843
Aaron Greenwald <aaron@aarongreenwald.com>
2021-09-22 12:07:30 +0300

    notes: gfm, tasklists support

--- web/src/notes/file-viewer.tsx
+++ web/src/notes/file-viewer.tsx
@@ -1,6 +1,8 @@
 import {DefaultButton, PrimaryButton} from '@fluentui/react';
 import ReactMarkdown from 'react-markdown';
 import * as React from 'react';
+import remarkGfm from 'remark-gfm';
+import styled from '@emotion/styled';
 
 export const FileViewer = ({content, onEdit, pendingCommit, onCommit}) => {
     return (
@@ -9,7 +11,17 @@ export const FileViewer = ({content, onEdit, pendingCommit, onCommit}) => {
             {
                pendingCommit && <DefaultButton onClick={onCommit}>Commit</DefaultButton>
             }
-            <ReactMarkdown>{content}</ReactMarkdown>
+            <StyledReactMarkdown remarkPlugins={[remarkGfm]}>{content}</StyledReactMarkdown>
         </>
     )
 }
+
+const StyledReactMarkdown = styled(ReactMarkdown)`
+  ul.contains-task-list {
+    padding-left: 1em;
+    
+    li {
+      list-style: none;  
+    }
+  }
+`
