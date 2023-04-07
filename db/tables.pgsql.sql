8adbfac1cce789b4084960aa2af5cb4483b69758
Aaron Greenwald <aaron@istraresearch.com>
2023-04-07 10:51:55 +0300

    WIP pgsql schema

--- db/tables.pgsql.sql
+++ db/tables.pgsql.sql
@@ -1,62 +1,110 @@
-﻿
+﻿/*
+ In order for the rollup logic to work right, there must be exactly one category ("uncategorized" or "root")
+ with a null parent_category_id
+ */
+CREATE TABLE category(category_id  integer primary key not null
+    ,name varchar(50) not null unique
+    ,parent_category_id integer null references category);
 
-CREATE TABLE fin_register(register_id serial primary key not null
-						,name varchar(50) NOT NULL UNIQUE);
-
-
-CREATE TABLE fin_category(category_id  serial primary key not null
-	,name varchar(50) NOT NULL UNIQUE
-	,register_id int NOT NULL REFERENCES fin_register);
-
---choice of table name is because it most closely represents the concept differentiating spending from income tables.
---payment/earning, debit/credit, do not capture the difference when it comes to returns and negative spending/income. 
---The pluralization is clumsy but not terrible. spending_item? blech.
---expenditure is better than spending but too clumsy
-CREATE TABLE fin_spending(spending_id  serial primary key not null
-					,paid_date date NOT NULL
-					,incurred_begin_date date NOT NULL
-					,incurred_end_date date NOT NULL
-					,recipient varchar(50) NOT NULL
-					,amount decimal(19,4) NOT NULL
-					,category_id int NOT NULL REFERENCES fin_category
-					,note text NULL					
-					);
-
-CREATE TABLE fin_income(income_id serial primary key not null
-				  , source varchar(50) NOT NULL
-				  , paid_date date NOT NULL
-				  , amount decimal(19,4) NOT NULL
-				  , register_id int NOT NULL REFERENCES fin_register
-				  , note text NULL
-				  );
-
-CREATE TABLE fin_cash_account(cash_account_id serial primary key not null
-			,name varchar( 50 )NOT NULL unique
-			);
-
-CREATE TABLE fin_cash_assets_record(cash_assets_record_id serial primary key not null
-			,record_date date NOT NULL
-			,cash_account_id int NOT NULL REFERENCES fin_cash_account
-			,amount decimal(19,4) NOT NULL
-			);
-
-CREATE TABLE fin_stock_transaction (stock_transaction_id serial primary key not null
-			,transaction_date date NOT NULL
-			,ticker_symbol varchar(20) NOT NULL
-			,unit_price decimal(19,4) NOT NULL
-			,quantity decimal(12,4) NOT NULL
-			,is_purchase boolean NOT NULL
-			);
-
-
-CREATE TABLE dry_entry(entry_id serial primary key not null
-			,start_datetime timestamp NOT NULL
-			,updated_datetime timestamp NOT NULL
-			,title varchar(128) NULL
-			,content text NULL
+CREATE TABLE payment(payment_id  integer primary key not null
+    ,paid_date date not null
+    ,incurred_begin_date date null --if empty assume the paid_date
+    ,incurred_end_date date null --if empty assume the incurred_begin_date
+    ,recipient varchar(50) not null
+    ,amount decimal(19,4) not null
+    ,incurred_amount decimal(19,4) not null
+    ,currency char(3) not null default 'ILS'
+    ,category_id int not null references category
+    ,note text null
+    --,cost decimal(19,4) null --only meaningful in the context of "items" table that should hang off this table
 );
 
-CREATE TABLE dry_entry_tag(entry_id int not null references dry_entry(entry_id)
-			,name varchar(20) NOT NULL
-			,primary key (entry_id, name)			
+CREATE TABLE income(income_id integer primary key not null
+    , source varchar(50) NOT NULL
+    , paid_date date NOT NULL
+    , amount decimal(19,4) NOT NULL
+    , currency char(3) not null default 'ILS'
+    , note text NULL
 );
+
+CREATE TABLE cash_account(cash_account_id integer primary key not null
+    ,name varchar( 50 )NOT NULL unique
+    ,currency char(3) NOT NULL default 'USD'
+    ,active boolean NOT NULL default true --when false, hidden from the input form in the UI, but everything else works as normal
+);
+
+CREATE TABLE cash_assets_record(cash_assets_record_id integer primary key not null
+    ,record_date date NOT NULL
+    ,cash_account_id int NOT NULL REFERENCES cash_account
+    ,amount decimal(19,4) NOT NULL
+    ,unique(record_date, cash_account_id)
+);
+
+CREATE TABLE stock_account(stock_account_id integer primary key not null
+    ,name varchar(50) NOT NULL unique
+    ,tax_category char(10) NOT NULL check ( tax_category in ('deferred', 'exempt', 'taxable') ));
+
+CREATE TABLE stock_transaction (stock_transaction_id integer primary key not null
+    /*
+        Consider changing this to a date, with timezone. As it stands, it is a timestamp but without timezone,
+        and represents the time in New York. Some of the historical data I have (automatic reinvestment of dividends)
+        seems to have been exported in Jerusalem time, which needs to be fixed.
+    */
+    ,transaction_date timestamp NOT NULL
+    ,account_id int NOT NULL REFERENCES stock_account
+    ,ticker_symbol varchar(20) NOT NULL
+    ,unit_price decimal(19,4) NOT NULL
+    ,quantity decimal(12,4) NOT NULL
+);
+
+create table stock_split(
+                            split_date date NOT NULL
+    , ticker_symbol varchar(20) NOT NULL
+    , previous_share_qty int NOT NULL
+    , new_share_qty int NOT NULL
+);
+
+create table cash_assets_allocation(cash_assets_allocation_id integer primary key not null
+    --consider a better name than record_date, since it's not a snapshot but a transaction
+    ,record_date date NOT NULL
+    ,allocation_code varchar(6) NOT NULL
+    ,currency char(3) NOT NULL
+    ,amount decimal(19,4) NOT NULL
+    ,note text NULL
+
+);
+
+create table financial_constants(
+                                    code varchar(10) not null primary key,
+                                    amount decimal(19,4) not null
+);
+
+create table fuel_log(
+                         fuel_log_id integer primary key not null
+    ,timestamp timestamp NOT NULL
+    ,odometer integer NOT NULL
+    ,liters decimal(6,4) NOT NULL
+    ,is_full boolean NOT NULL DEFAULT TRUE
+    ,note text NULL
+    ,payment_id integer NOT NULL REFERENCES payment
+);
+
+create table file(
+    -- md5?
+                     sha256 text primary key not null,
+                     sha1 text not null, --todo check that this is 1:1 on the pk
+                     bytes integer not null,
+                     accessed integer null,
+                     created integer null,
+                     mimetype text null,
+                     storage_account_1 boolean not null default false,
+                     storage_account_2 boolean not null default false
+);
+
+create table filename(
+                         name text not null,
+                         created timestamp null, --this can also be thought of as the modified date on the file
+                         sha256 char(64) not null references file,
+                         accessed timestamp null,
+                         unique(name, created) --primary key - this is a "file version"
+)
