c395c6e12200222e360dc17d801c79175a04fefc
Aaron Greenwald <aaron@aarongreenwald.com>
2023-06-21 20:18:49 +0300

    Show full category name in monthly_spending script

--- db/scripts/monthly_spending.sql
+++ db/scripts/monthly_spending.sql
@@ -7,14 +7,10 @@ with_month as (
       cross join decl
       inner join v_rollup_categories c on p.category_id = c.category_id and c.root_category_id = decl.root_cat
       -- add where for specific categories to further filter specific categories within the root category,
-      -- or completely remove the root cat filter and join specific categories
-      
+      -- or completely remove the root cat filter and join specific categories     
       union all
-
       select strftime('%Y%m', end_date / 1000,  'unixepoch') month, ils, usd
       from v_unreported_spending
-          
-            
     )
     select month, round(sum(ils)) ils, round(sum(usd)) usd
     from with_month
@@ -35,15 +31,11 @@ with_month as (
      select strftime('%Y%m', paid_date / 1000,  'unixepoch') month, c.full_name category, ils, usd
      from v_payment p
      cross join decl
-     inner join v_rollup_categories c on p.category_id = c.category_id and c.root_category_id = decl.root_cat
-      
-      union all
-
-      select strftime('%Y%m', end_date / 1000,  'unixepoch') month, 'Unreported', ils, usd
-      from v_unreported_spending
-     
-            
-    )
+     inner join v_rollup_categories c on p.category_id = c.category_id and c.root_category_id = decl.root_cat      
+     union all
+     select strftime('%Y%m', end_date / 1000,  'unixepoch') month, 'Unreported', ils, usd
+     from v_unreported_spending
+)
 select month, category, round(sum(ils)) ils, round(sum(usd)) usd
 from with_month inner join decl on with_month.month = decl.mnth
 group by month, category
