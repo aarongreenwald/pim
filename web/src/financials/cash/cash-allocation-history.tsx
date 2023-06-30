e4dcf04fc701b87770c8794ab175de2ac6aa7a07
Aaron Greenwald <aaron@aarongreenwald.com>
2023-06-30 09:27:40 +0300

    Accidentally missed file in previous commit, oops :-(

---
+++ web/src/financials/cash/cash-allocation-history.tsx
@@ -0,0 +1,20 @@
+import {PanelProps} from '../../common/panel.types';
+import * as React from 'react';
+import {useEffect, useState} from 'react';
+import {CashAssetAllocationHistory} from '@pim/common';
+import {getCashAssetAllocationHistory} from '../../services/server-api';
+import {List} from '../list';
+
+export const CashAllocationHistory: React.FC<PanelProps<string>> = ({id}) => {
+    const [allocationHistory, setAllocationHistory] = useState<CashAssetAllocationHistory[]>(null)
+    useEffect(() => {
+        getCashAssetAllocationHistory(id).then(setAllocationHistory);
+    }, [id])
+
+
+    return (
+	allocationHistory &&
+	<List<CashAssetAllocationHistory>
+            data={allocationHistory} />
+    )
+}
