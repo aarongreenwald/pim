302a01fbab46c4b82da43812585f41649074fd04
Aaron Greenwald <aaron@aarongreenwald.com>
2024-04-22 09:14:55 +0300

    drought readme/todo

--- README.md
+++ README.md
@@ -2,6 +2,16 @@ For running web locally you need GITHUB_TOKEN on the path but it's not used, so 
 
 [![pim-web](https://github.com/aarongreenwald/pim/actions/workflows/web.yml/badge.svg)](https://github.com/aarongreenwald/pim/actions/workflows/web.yml)
 
+### Overview
+
+* *db* - to be accessed _only_ via the server
+* *server*: nodejs webserver, serves all frontends
+* *web*: React frontend
+* *emacs*: Emacs frontend
+* *drought*: Utility for loading drive data into PIM
+* *common*: Typescript shared by server and web
+* *archive*: Delete this? 
+
 ### Currency Support
 
 * The db tables are currency agnostic, _except_ `fx_transaction` which assumes a constant home currency. Still, it doesn't specify what it is. 
@@ -10,3 +20,4 @@ For running web locally you need GITHUB_TOKEN on the path but it's not used, so 
 ### TODO
 
 * Nightly marketdata
+* DIP (Docs-in-PIM) - see `./drought/README.md`
--- drought/files.py
+++ drought/files.py
@@ -9,36 +9,6 @@ from operator import itemgetter
 from dotenv import load_dotenv
 from b2sdk.v2 import *
 
-"""
-
-TODO
-
-* Connect to the remote PIM db so a copy isn't required for this. Latency will be an issue so consider this carefully.
-* Decide how to handle git/svn repos. 
-   * Review skipped repos
-   * Log/document structure of the zipped repos
-* Deduping repos
-* Cleaning files
-* Build frontend
-* mimetypes might be able to be improved. 
-* Find a better method to upload large files so that I can validate the hashes. Perhaps the lower level API will allow this?
-* Performance:
-  * For files that need to be uploaded, obviously the upload dominates and I'm limited by bandwidth, so parallelization isn't worth much
-  * For subsequent runs, the db updates are expensive so it's important to avoid unnecessary updates. 
-   * After db updates, it seems the primary cost is hashing large files. I could limit to only sha256. But it's a relatively small cost
-   * How much is the logging costing in perf? Not much, but measure it. 
-   * I'm not sure how db inserts compare to updates, but the reads seem very fast. Still, I could load the entiredb into memory at the start and not 
-hit the disk again unless there's a change. 
-   * The `find` is trivial. 
-* Dry run mode - document required updates and exit.
-
-
-Notes
-
-* Small files have a sha1 in backblaze, large files don't but they have a large_file_sha1 field in the fileinfo dict
-
-"""
-
 DRY_RUN = False
 BUF_SIZE = 65536  # 64kb chunks
 VERBOSE = True
